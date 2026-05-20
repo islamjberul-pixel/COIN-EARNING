242f6d817ddc10fa74a429096c4cd33104750d48
islamjberul-pixel <islamjberul@gmail.com>
2026-05-20 09:53:06 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11026455
+    "zoneId": 11030855
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
